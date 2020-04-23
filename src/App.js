f0130ef189a3e2a601a9e1224795b46b96b9a27f
yago-codev <yagomilano92@gmail.com>
2020-04-23 02:06:13 -0300

    Projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,141 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+import { ReactComponent as AlienIcon } from './assets/alien.svg';
+import { ReactComponent as DownIcon } from './assets/down-arrow.svg';
+import { ReactComponent as LeftIcon } from './assets/down-arrow.svg';
+import { ReactComponent as RightIcon } from './assets/down-arrow.svg';
+import { ReactComponent as ConfigIcon } from './assets/options.svg';
+import { ReactComponent as CatIcon } from './assets/cat.svg';
+import { ReactComponent as KoalaIcon } from './assets/koala.svg';
+import { ReactComponent as DuckIcon } from './assets/platypus.svg';
+import { ReactComponent as CockIcon } from './assets/hen.svg';
+import { ReactComponent as HedgehogIcon } from './assets/hedgehog.svg';
+
+
 
 function App() {
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        {props.children}
+      </a>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <NavBar>
+      <NavItem icon={<CockIcon />} />
+      <NavItem icon={<ConfigIcon />} />
+      <NavItem icon={<DownIcon />}>
+        <DropdownMenu />
+      </NavItem>
+    </NavBar>
+  );
+}
+
+function DropdownMenu() {
+  const [menuAtivo, setMenuAtivo] = useState('main');
+  const [menuAltura, setMenuAltura] = useState(null);
+
+  function calcularAltura(elemento) {
+    const altura = elemento.offsetHeight;
+    setMenuAltura(altura);
+  }
+
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item" onClick={() => props.vaParaOMenu && setMenuAtivo(props.vaParaOMenu)}>
+        <span className="icon-button">
+          {props.leftIcon}
+        </span>
+
+        {props.children}
+
+        <span className="icon-right">
+          {props.rightIcon}
+        </span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown" style={{ height: menuAltura }}>
+      <CSSTransition
+        in={menuAtivo === 'main'}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-primary"
+        onEnter={calcularAltura}
+      >
+        <div className="menu">
+          <DropdownItem leftIcon={<AlienIcon />}>
+            Meu Perfil
+          </DropdownItem>
+
+          <DropdownItem
+            leftIcon={<LeftIcon />}
+            vaParaOMenu="settings"
+          >
+            Animais
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={menuAtivo === 'settings'}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-secondary"
+        onEnter={calcularAltura}
+      >
+        <div className="menu">
+          <DropdownItem
+            leftIcon={<RightIcon />}
+            vaParaOMenu="main"
+          />
+          <DropdownItem leftIcon={<CatIcon />}>
+            Gatinho
+          </DropdownItem>
+
+          <DropdownItem leftIcon={<KoalaIcon />}>
+            Koala
+          </DropdownItem>
+
+          <DropdownItem leftIcon={<DuckIcon />}>
+            Pato
+          </DropdownItem>
+
+          <DropdownItem leftIcon={<HedgehogIcon />}>
+            Porco Espinho
+          </DropdownItem>
+        </div>
+      </CSSTransition>
     </div>
+  );
+}
+
+function NavBar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        {props.children}
+      </ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+  const [abrir, setAbrir] = useState(false);
+
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button" onClick={() => setAbrir(!abrir)}>
+        {props.icon}
+      </a>
+
+      {abrir && props.children}
+    </li>
   );
 }
 
